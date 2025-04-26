3b4a8234ed34ece4f1f2a7dbaab4391ab16751cf
Alxhr0 <alxhr0@proton.me>
2025-04-26 03:28:27 +0200

    🔧 Fix nixos.rs

--- src/modules/nixos.rs
+++ src/modules/nixos.rs
@@ -11,7 +11,6 @@ pub fn running_on_nixos() -> bool {
     if Path::new("/run/current-system/sw/bin").exists() {
         true
     } else {
-        false;
-        std::process::exit(1)
+        false
     }
 }
