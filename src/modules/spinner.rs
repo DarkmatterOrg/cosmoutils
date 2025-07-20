8f65efa8dc3327d90462b48f77b3e465ed3c1e87
Alxhr0 <alxhr0@proton.me>
2025-07-20 18:44:13 +0200

    🔧 Add create_spinner

--- src/modules/spinner.rs
+++ src/modules/spinner.rs
@@ -1,17 +1,21 @@
+use std::str::FromStr;
 use spinners::{Spinner, Spinners};
 use std::thread::sleep;
 use std::time::Duration;
 
-/// Create a Dots12 spinner
+/// Create a spinner
 /// ```rust
 ///     use cosmoutils::modules::spinner::create_spinner;
 ///
 ///     fn main() {
-///         create_spinner(10, "Loading...");
+///         create_spinner(10, "Loading...", "Dots12");
 ///     }
 /// ```
-pub fn create_spinner(seconds: u64, message: &str) {
-    let mut sp = Spinner::new(Spinners::Dots12, message.into());
+pub fn create_spinner(seconds: u64, message: &str, spinner_name: &str) {
+    let spinner_enum = Spinners::from_str(spinner_name)
+        .unwrap_or(Spinners::Dots12);
+
+    let mut sp = Spinner::new(spinner_enum, message.into());
     sleep(Duration::from_secs(seconds));
     sp.stop();
 
